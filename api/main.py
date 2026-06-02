2d863b81c3c1fe7952dae023d189c69259beb4df
nevzlad <nevzlad@gmail.com>
2026-06-02 18:30:16 +0300

    Revert "fix: add timeout to bot.get_me() in debug endpoint to prevent hang"
    This reverts commit 092adbd19ddf133aed6a851bcbd3cde3217ca775.

--- api/main.py
+++ api/main.py
@@ -125,7 +125,7 @@ async def debug():
     bot_info = None
     if bot_mod.bot:
         try:
-            me = await asyncio.wait_for(bot_mod.bot.get_me(), timeout=5)
+            me = await bot_mod.bot.get_me()
             bot_info = {"id": me.id, "username": me.username, "first_name": me.first_name}
         except Exception as e:
             bot_info = {"error": str(e)[:200]}
